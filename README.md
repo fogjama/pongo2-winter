64c7c3ca2bc66edb99938ccc3b0549bf12492305
fogjama <66557609+fogjama@users.noreply.github.com>
2020-11-20 21:37:07 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Pongo's Adventure 2: Snow Snafu
+
+Pongo's Adventure 2 builds on the pracice elements from Pongo's Adventure with a focus on cleaner, more general code that can be reused to generte additional levels with minimal adjustment.
---
+++ objects.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+from pygame import freetype
+import os
+from constants import GREEN
+
+class Player(pygame.sprite.Sprite):
+    pass
+
+class Monster(pygame.sprite.Sprite):
+    pass
+
+class Platform(pygame.sprite.Sprite):
+    pass
+
+class Loot(pygame.sprite.Sprite):
+    pass
+
+class EnvObject(pygame.sprite.Sprite):
+    pass
+
+class Projectile(pygame.sprite.Sprite):
+    pass
